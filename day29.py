463682be3e6f954fc96f36dfcfcede00c5b52e45
SmriteePoudel <esmritypoudel@gmail.com>
2026-05-09 23:25:32 +0545

    args function

--- day29.py
+++ day29.py
@@ -9,3 +9,16 @@ calc = Calculator()
 print(calc.add(5))        # one argument
 print(calc.add(5, 10))    # two arguments
 print(calc.add(5, 10, 15))# three arguments
+
+#Using args
+
+class Calculator:
+    def add(self, *numbers):
+        return sum(numbers)
+
+calc = Calculator()
+
+print(calc.add(5))          # one argument
+print(calc.add(5, 10))      # two arguments
+print(calc.add(5, 10, 15))  # three arguments
+print(calc.add(1, 2, 3, 4)) # many arguments
